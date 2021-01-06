d783a1ad3a220dd2313b278422f1d5949e24444c
mscbr <scibor.maciej@gmail.com>
2021-01-06 20:05:05 +0100

    Solves [Chat Layout]

--- src/components/messageInput/index.tsx
+++ src/components/messageInput/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Textarea } from '@chakra-ui/react';
+import { Button, Textarea, Container } from '@chakra-ui/react';
 
 interface Props {
   value?: string;
@@ -9,17 +9,30 @@ interface Props {
 }
 
 const MessageInput: React.FC<Props> = ({ value, onChange, onSend }) => {
+  const handleKeyDown = ({ key }: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (key === 'Enter') onSend();
+  };
   return (
-    <>
+    <Container
+      minH="100%"
+      maxW="100%"
+      padding={0}
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
       <Textarea
         value={value}
         onChange={onChange}
         placeholder="Your message..."
+        onKeyDown={handleKeyDown}
+        isFullWidth={true}
+        flexGrow={1}
       />
-      <Button onClick={onSend} size="sm" variant="outline">
+      <Button onClick={onSend} size="sm" variant="outline" mt={4}>
         S E N D
       </Button>
-    </>
+    </Container>
   );
 };
 
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
 
 import MessageInput from 'components/messageInput';
 
 const Chat: React.FC = () => {
+  const upSm = useBreakpointValue({ base: false, md: true });
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
   const handleSendMessage = () => {
@@ -10,18 +12,37 @@ const Chat: React.FC = () => {
     setMessage('');
   };
   return (
-    <>
-      <MessageInput
-        value={message}
-        onChange={({ target: { value } }) => setMessage(value)}
-        onSend={handleSendMessage}
-      />
-      <ul>
-        {messages.map((item, i) => (
-          <li key={`${i}-item`}>{item}</li>
-        ))}
-      </ul>
-    </>
+    <Grid
+      h="100vh"
+      templateColumns={upSm ? '1fr 2fr' : '1fr'}
+      templateRows="3fr 1fr 1fr"
+      gap={1}
+      color="white"
+      background="white"
+      border="2px solid white"
+      borderRadius={5}
+    >
+      <GridItem bg="black" p={8} display={!upSm && 'none'}>
+        USER LIST
+      </GridItem>
+      <GridItem bg="black" p={8} rowSpan={2}>
+        <ul>
+          {messages.map((item, i) => (
+            <li key={`${i}-item`}>{item}</li>
+          ))}
+        </ul>
+      </GridItem>
+      <GridItem bg="black" p={8} rowSpan={2} display={!upSm && 'none'}>
+        USER PROFILE/CAMERA
+      </GridItem>
+      <GridItem p={2} bg="black">
+        <MessageInput
+          value={message}
+          onChange={({ target: { value } }) => setMessage(value)}
+          onSend={handleSendMessage}
+        />
+      </GridItem>
+    </Grid>
   );
 };
 
