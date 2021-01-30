76b7c62ed691526cb19ea214550a8332f9f32291
mscbr <scibor.maciej@gmail.com>
2021-01-30 12:22:48 +0100

    Solves [Video feedback]

--- client/src/components/videoFeedback/index.tsx
+++ client/src/components/videoFeedback/index.tsx
@@ -1,77 +1,95 @@
-import React, { useState, useRef } from 'react';
+import React, { useCallback, useRef, useEffect, useState } from 'react';
 import * as faceapi from 'face-api.js';
+import { Spinner } from '@chakra-ui/react';
 
 import { useUserMedia } from 'hooks/useUserMedia';
 import './index.css';
 
 const CAPTURE_OPTIONS = {
   audio: false,
-  video: { facingMode: 'environment' }
+  video: { facingMode: 'user' }
 };
 
 const VideoFeedback = () => {
-  // const [mediaStream, setMediaStream] = useState(null);
-  // const [loading, setLoading] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D>();
   const mediaStream = useUserMedia(CAPTURE_OPTIONS);
 
   if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
     videoRef.current.srcObject = mediaStream as MediaStream;
   }
 
-  const handleCanPlay = () => {
+  const handleCanPlay = useCallback(() => {
     videoRef.current?.play();
-  };
+  }, [videoRef]);
 
-  // Promise.all([
-  //   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  //   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  //   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  //   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-  // ]).then(() => {
-  //   setLoading(false);
-  //   startVideo();
-  // });
+  useEffect(() => {
+    let renderDetection: NodeJS.Timeout;
+    if (videoRef.current && canvasRef.current) {
+      const renderCtx = canvasRef.current.getContext('2d');
+      if (renderCtx) setContext(renderCtx);
+      if (context) {
+        faceapi.matchDimensions(canvasRef.current, {
+          width: canvasRef.current.width,
+          height: canvasRef.current.height
+        });
+        renderDetection = setInterval(async () => {
+          const detections = await faceapi
+            .detectAllFaces(
+              //@ts-expect-error
+              videoRef.current,
+              new faceapi.TinyFaceDetectorOptions()
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions();
+          const resizedDetections = faceapi.resizeResults(detections, {
+            //@ts-expect-error
+            width: canvasRef.current.width,
+            //@ts-expect-error
+            height: canvasRef.current.height
+          });
+          context.clearRect(
+            0,
+            0,
+            //@ts-expect-error
+            canvasRef.current.width,
+            //@ts-expect-error
+            canvasRef.current.height
+          );
+          //@ts-expect-error
+          faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+          faceapi.draw.drawFaceLandmarks(
+            //@ts-expect-error
+            canvasRef.current,
+            resizedDetections
+          );
+          faceapi.draw.drawFaceExpressions(
+            //@ts-expect-error
+            canvasRef.current,
+            resizedDetections
+          );
+        }, 100);
+      }
 
-  const handlePlay = () => {
-    console.log('playing');
-    // if (videoRef.current) {
-    //   const canvas = faceapi.createCanvasFromMedia(videoRef.current);
-    //   document.body.append(canvas);
-    //   const displaySize = {
-    //     width: videoRef.current.width,
-    //     height: videoRef.current.height
-    //   };
-    //   faceapi.matchDimensions(canvas, displaySize);
-    //   setInterval(async () => {
-    //     const detections = await faceapi
-    //       .detectAllFaces(
-    //         videoRef.current as HTMLVideoElement,
-    //         new faceapi.TinyFaceDetectorOptions()
-    //       )
-    //       .withFaceLandmarks()
-    //       .withFaceExpressions();
-    //     const resizedDetections = faceapi.resizeResults(
-    //       detections,
-    //       displaySize
-    //     );
-    //     canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
-    //     faceapi.draw.drawDetections(canvas, resizedDetections);
-    //     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    //     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-    //   }, 100);
-    // }
-  };
+      return () => clearInterval(renderDetection);
+    }
+  }, [videoRef.current, canvasRef.current, context]);
 
-  return (
-    <video
-      onCanPlay={handleCanPlay}
-      ref={videoRef}
-      id="video-stream"
-      autoPlay
-      playsInline
-      muted
-    />
+  return mediaStream ? (
+    <div id="video-wrapper">
+      <video
+        onCanPlay={handleCanPlay}
+        ref={videoRef}
+        id="video-stream"
+        autoPlay
+        playsInline
+        muted
+      />
+      <canvas id="canvas" ref={canvasRef} />
+    </div>
+  ) : (
+    <Spinner />
   );
 };
 
